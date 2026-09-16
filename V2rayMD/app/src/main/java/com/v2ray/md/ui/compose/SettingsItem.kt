8a5d8eb4fb9b1088bf1c72e19f8079b47656b880
wectazz <wectazz@gmail.com>
2026-09-17 00:22:58 +0300

    Fix missing Arrangement import in SettingsItem

--- V2rayMD/app/src/main/java/com/v2ray/md/ui/compose/SettingsItem.kt
+++ V2rayMD/app/src/main/java/com/v2ray/md/ui/compose/SettingsItem.kt
@@ -1,6 +1,7 @@
 package com.v2ray.md.ui.compose
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
