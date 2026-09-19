080557c4441c24491e18a8ea46a91a37ce123f9b
wectazz <wectazz@gmail.com>
2026-09-20 01:28:30 +0300

    Remove background from ButtonGroup as requested

--- V2rayMD/app/src/main/java/com/v2ray/md/ui/compose/Components.kt
+++ V2rayMD/app/src/main/java/com/v2ray/md/ui/compose/Components.kt
@@ -464,7 +464,7 @@ fun SingleSelectButtonGroup(
     modifier: Modifier = Modifier
 ) {
     androidx.compose.material3.ButtonGroup(
-        modifier = modifier.background(MaterialTheme.colorScheme.surfaceContainerHighest, androidx.compose.foundation.shape.CircleShape),
+        modifier = modifier,
         overflowIndicator = {}
     ) {
         options.forEachIndexed { index, label ->
