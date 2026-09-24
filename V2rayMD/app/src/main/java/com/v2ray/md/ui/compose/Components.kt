5836a0fcdb6439b0b6d1aeb6d8718eb52d276deb
wectazz <wectazz@gmail.com>
2026-09-25 00:28:24 +0300

    Migrate Coil 2 to Coil 3

--- V2rayMD/app/src/main/java/com/v2ray/md/ui/compose/Components.kt
+++ V2rayMD/app/src/main/java/com/v2ray/md/ui/compose/Components.kt
@@ -86,8 +86,8 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
 import com.v2ray.md.R
 import com.v2ray.md.util.AppIconFetcher
 import sh.calvin.reorderable.ReorderableCollectionItemScope
